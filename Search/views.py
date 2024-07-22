d50f44b709ad8ceadc35b1f5e89cd8acaf59772f
Darkus03108 <kdayushbornin2004@gmail.com>
2024-07-23 00:36:48 +0530

    added bg

--- Search/views.py
+++ Search/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django import forms
 # Create your views here.
+
+class searchForm(forms.Form):
+    query = forms.CharField(label="Enter your query here!")
+
+
 
 def index(request):
     return render(request, "src/index.html")
